d31bcd8764f8bcce52ce5bc394f4083fba5dbc79
Patrick Porto <patrick.s.porto@gmail.com>
2023-11-26 15:46:01 -0700

    chore: setup combat tracker

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { MODULE_NAME } from "./constants.js";
+import "./ose-combat-tracker.css";
 
 Hooks.on('init', async () => {
+    if (!game.modules.get("petitevue-lib")?.active) {
+        ui.notifications.error("PetiteVue is not installed or not active. Please install and activate it to use this module.")
+        return
+    }
     console.log(`${MODULE_NAME} | Initializing ${MODULE_NAME}`);
 });
